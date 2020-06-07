342bfca43269d0621672aca4cd77b72f9b8c1dcc
SidraShaikh-2 <57295469+SidraShaikh-2@users.noreply.github.com>
2020-06-07 14:42:16 +0500

    chapter 9-11

--- Javascript Assignment/app9-11.js
+++ Javascript Assignment/app9-11.js
@@ -11,6 +11,7 @@ else{
 }
 
 
+
 // 2-Write a program to take “gender” as input from user.
 //  If the user is male, give the message:  Good Morning Sir.
 //  If the user is female, give the message: Good Morning Ma’am.
